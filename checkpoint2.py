27bc03fab4608fa58ccf41bfa1edd33ac399fe9e
Amarachi Uzochukwu <144688477+Anikwendu@users.noreply.github.com>
2023-11-04 23:59:52 +0100

    Add files via upload

---
+++ checkpoint2.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from PIL import Image
+import pickle
+import sklearn
+import streamlit as st
+my_model = pickle.load(open("C:\\Users\\Amarachi Uzochukwu\\Desktop\\rf_model.sav", "rb"))
+st.title("BANK ACCOUNT PREDICTION APP")
+img = Image.open("C:\\Users\\Amarachi Uzochukwu\\Downloads\\pexels-anna-shvets-4482900.jpg")
+st.image(img, width=350)
+def user_report():
+    country = st.sidebar.slider('country', 0.0, 4.0, 1.0)
+    cellphone_access = st.sidebar.slider('cellphone_access', 0.0, 2.0, 1.0)
+    education_level = st.sidebar.slider('education_level', 0.0, 8.0, 1.0)
+    job_type = st.sidebar.slider('job_type', 0.0, 12.0, 0.1)
+
+    input_data = {
+        'country': country,
+        'cellphone_access': cellphone_access,
+        'education_level': education_level,
+        'job_type': job_type
+    }
+    data = pd.DataFrame(input_data, index=[0])
+    return data
+
+user_data = user_report()
+st.write(user_data)
+prediction = my_model.predict(user_data)
+if (prediction==0):
+    st.success('Yes')
+else:
+    st.success('No')
+
